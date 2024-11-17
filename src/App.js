5955de3203325500e1a6523a6278b72f1f134626
santhossunthar <santhoshsunthar@gmail.com>
2024-11-18 00:34:14 +0530

    Initiated app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Dairy Farmers!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
