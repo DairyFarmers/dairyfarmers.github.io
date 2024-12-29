4c8bbba9ab91eb64db4cd17906cd4dade6b84db3
Tharsan <sivatharshan81@gmail.com>
2024-12-29 15:27:58 +0530

    Add dashboard

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { axiosPrivate } from './api/axios';
 import { token_verification_path } from './api/config';
-import { PrivateRoute } from "./Components/common/PrivateRoute";
+import { PrivateRoute } from "./Components/Common/PrivateRoute";
 import Login from "./pages/Signin/SignIn";
-// import AdminLogin from "./pages/AdminSignin/AdminLogin";
-import { useState } from "react";
-import Dashboard from "./pages/Home/Home";
+import Home from "./pages/Home/Home";
 import Error from "./pages/Error";
 import { login } from "./redux/slices/userSlice";
 
@@ -19,32 +17,29 @@ const App = () => {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axiosPrivate.get(token_verification_path);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const response = await axiosPrivate.get(token_verification_path);
 
-        if (response.status === 200) {
-          dispatch(login(response.data.data));
-        }
-      } catch (err) {
-        navigate('/', {
-          state: { from: location },
-          replace: true
-        });
-      }
-    }
-    fetchData();
-  }, [isLoggedIn]);
+  //       if (response.status === 200) {
+  //         dispatch(login(response.data.data));
+  //       }
+  //     } catch (err) {
+  //       navigate('/', {
+  //         state: { from: location },
+  //         replace: true
+  //       });
+  //     }
+  //   }
+  //   fetchData();
+  // }, [isLoggedIn]);
 
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/error" element={<Error />} />
-      <Route path="/dashboard" element={
-        <PrivateRoute>
-          <Dashboard />
-        </PrivateRoute>} />
+      <Route path="/dashboard" element={<Home />}/>
     </Routes>
   );
 }
