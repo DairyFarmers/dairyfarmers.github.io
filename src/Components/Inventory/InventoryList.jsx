163268ba869e770e4adf96fe64b16e80456df675
santhossunthar <santhoshsunthar@gmail.com>
2025-04-26 21:02:35 +0530

    Added API integration for inventory list component

--- src/Components/Inventory/InventoryList.jsx
+++ src/Components/Inventory/InventoryList.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import InventoryListTable from "../tables/InventoryListTable";
+import { axiosPrivate } from '../../api/axios';
+import { inventory_list_path } from '../../api/config';
+import { useNavigate } from 'react-router-dom';
 
 const InventoryList = () => {
+  const navigate = useNavigate();
   const [selectedItem, setSelectedItem] = useState(null);
+  const [inventoryItems, setInventoryItems] = useState([]);
 
-  const handleEdit = (item) => {
-    setSelectedItem(item);
-    console.log("Editing Item:", item);
+  const getInventoryItems = async (item) => {
+        try {
+          const response = await axiosPrivate.get(inventory_list_path);
+    
+          if (response.status === 200) {
+            setInventoryItems(response.data);
+          }
+        } catch (error) {
+            navigate("/error", { replace: true });
+            console.error("Error fetching inventory items:", error);
+        }
   };
+
+  useEffect(() => {
+    getInventoryItems();
+  }, []);
 
   const inventoryData = [
     {
@@ -36,7 +53,7 @@ const InventoryList = () => {
   return (
     <div className="container mt-4 p-4">
       <h2 className="font-bold mb-4 title">Inventory Items</h2>
-      <InventoryListTable data={inventoryData} />
+      <InventoryListTable data={inventoryItems} />
     </div>
   );
 };
